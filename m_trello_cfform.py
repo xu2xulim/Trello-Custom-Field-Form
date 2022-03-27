dfa80c7842d9ca1c527534a9af4ede23689066ed
Sunny Lim <limhss@gmail.com>
2022-03-27 15:26:16 +0800

    S

--- m_trello_cfform.py
+++ m_trello_cfform.py
@@ -24,10 +24,11 @@ with st.form("Configure Trello Dynamic Custom Field Form"):
         st.session_state['cfd'] = {}
 
 
-collect = {}
+
 with st.form("Trello Dynamic Custom Field Form"):
     st.write("The form is dynamically created based on the custom field definitions of any Trello Board")
     cfd = st.session_state['cfd']
+    collect = {}
     for df in cfd:
         if df['type'] == 'text' :
             collect[df['name']] = st.text_input(df['name'])
