ee29b2d1cb13f38b9a79437363f519284ab304fa
Sunny Lim <limhss@gmail.com>
2022-03-27 10:29:14 +0800

    S

--- m_trello_cfform.py
+++ m_trello_cfform.py
@@ -5,6 +5,7 @@ import numpy as np
 from datetime import datetime
 from deta import Deta
 import json
+import httpx
 
 res = httpx.post('https://70297.wayscript.io/function5')
 cfd = res.json()['cfd']
