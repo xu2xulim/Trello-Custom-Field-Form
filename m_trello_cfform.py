9b50466ba0abbdc2bf01ce82d7bdabdba55099f1
Sunny Lim <limhss@gmail.com>
2022-03-27 16:24:57 +0800

    S

--- m_trello_cfform.py
+++ m_trello_cfform.py
@@ -22,10 +22,7 @@ with st.form("Configure Trello Dynamic Custom Field Form"):
         st.session_state['cfd'] = cfd
 
 
-if 'collect' in st.session_state.keys():
-    pass
-else:
-    st.session_state['collect'] = {}
+
 collect = {}
 with st.form("Trello Dynamic Custom Field Form"):
     st.write("The form is dynamically created based on the custom field definitions of any Trello Board")
@@ -50,14 +47,16 @@ with st.form("Trello Dynamic Custom Field Form"):
     ready = st.form_submit_button("Submit")
 
     if ready:
-        st.session_state['collect'] = collect
-        st.json(st.session_state['collect'])
+        st.write(collect)
         #st.write("slider", slider_val, "checkbox", checkbox_val)
 
 st.write("Outside the form")
 st.header('Above is the json output generated')
+"""
 st.header('You can incorporate other cool things like')
 st.camera_input('Test Camera')
 st.file_uploader('Upload any file up to 200MB')
 st.color_picker('Pick a color')
 st.write('Please note that this is demo and the data is not capture in Trello')
+
+"""
