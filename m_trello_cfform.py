49b8511467405ebe06f53cd990a0b4bcd54b7c8b
Sunny Lim <limhss@gmail.com>
2022-03-28 21:41:36 +0800

    S

--- m_trello_cfform.py
+++ m_trello_cfform.py
@@ -52,18 +52,15 @@ with st.form("Trello Dynamic Custom Field Form", clear_on_submit=True):
 
 
 st.write("Outside the form")
-filename = ""
-filename = st.text_input('What is the filename?')
 
-if filename != "":
-    uploaded_file = st.file_uploader('Upload any file up to 200MB')
-    attach = {}
-    if uploaded_file is not None:
+uploaded_file = st.file_uploader('Upload any file up to 200MB')
+attach = {}
+if uploaded_file is not None:
 
-        bytes_data = uploaded_file.getvalue()
-        attach['card_id'] = st.session_state['card_id']
-        attach['filename'] = filename
-        res_attach = requests.post('https://bpqc1s.deta.dev/attach', data=attach, files = {'upload_file': bytes_data})
+    bytes_data = uploaded_file.getvalue()
+    attach['card_id'] = st.session_state['card_id']
+    attach['filename'] = uploaded_file.name
+    res_attach = requests.post('https://bpqc1s.deta.dev/attach', data=attach, files = {'upload_file': bytes_data})    
 
 ## Test
 """
