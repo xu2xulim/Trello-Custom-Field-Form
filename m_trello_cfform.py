cb1ce5c35bd07ea545d2e34f9bdd57d15f271369
Sunny Lim <limhss@gmail.com>
2022-03-28 15:59:15 +0800

    S

--- m_trello_cfform.py
+++ m_trello_cfform.py
@@ -46,7 +46,7 @@ with st.form("Trello Dynamic Custom Field Form", clear_on_submit=True):
         res = requests.post('https://bpqc1s.deta.dev/update', json=collect)
         if res.status_code == 200:
             st.balloons()
-            ready == False
+            res = {}
         else:
             st.error(res.text)
 
