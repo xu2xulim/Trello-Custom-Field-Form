a04a96a3c531879269fc7b0120f654fda76de736
Sunny Lim <limhss@gmail.com>
2022-03-28 10:06:36 +0800

    S

--- m_trello_cfform.py
+++ m_trello_cfform.py
@@ -13,9 +13,8 @@ board_id = st.sidebar.selectbox(
 
 st.title("Trello Dynamic Custom Field Form and other cool stuff")
 
-res = httpx.post('https://bpqc1s.deta.dev/get_definitions?board_id={}'.format(board_id)) #st.write("slider", slider_val, "checkbox", checkbox_val)
-#cfd = res.json()['cfd']
-st.write(res.text)
+res = httpx.get('https://bpqc1s.deta.dev/get_definitions?board_id={}'.format(board_id)) #st.write("slider", slider_val, "checkbox", checkbox_val)
+cfd = res.json()['cfd']
 st.session_state['cfd'] = cfd
 
 
