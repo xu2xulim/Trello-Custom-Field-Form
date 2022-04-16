b4aff880f236b52374f35da64647bd2cdb9c89a2
Sunny Lim <limhss@gmail.com>
2022-04-16 21:47:04 +0800

    S

--- m_trello_deta_form.py
+++ m_trello_deta_form.py
@@ -7,10 +7,9 @@ from deta import Deta
 import json
 import requests
 import base64
-board_id = st.sidebar.selectbox(
-    "Select a board",
-    ("5fdd53039a97d380e792101e", "5fdd5958823f7d04004f236f", "61120a2d004a725ed3f7f0db")
-)
+
+board_id = "61120a2d004a725ed3f7f0db"
+
 
 order = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG").Base("trello_orders")
 if 'more' in st.session_state :
