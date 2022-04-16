7cbe09963f5220fd8461177fee4e2d21b7b83534
Sunny Lim <limhss@gmail.com>
2022-04-16 20:03:08 +0800

    S

--- m_trello_deta_form.py
+++ m_trello_deta_form.py
@@ -13,60 +13,7 @@ board_id = st.sidebar.selectbox(
 )
 
 order = Deta("c0vidk60_8unssenvnHkuZmQfqhZ4jW49o5hRMvwG").Base("trello_orders")
-cfd = {}
-card_id = None
-st.title("Trello Form on Deta")
-"""#changed to requests
-res_get = requests.get('https://bpqc1s.deta.dev/get_definitions?board_id={}'.format(board_id)) #st.write("slider", slider_val, "checkbox", checkbox_val)
-cfd = res_get.json()['cfd']
 
-form1 = st.form("Trello Order with Deta", clear_on_submit=True)
-collect = {}
-collect['board_id'] = board_id
-collect['cardname'] =form1.text_input('Card Name')
-collect['carddescription'] = form1.text_area('Card Description')
-form1.write("The form is dynamically created based on the custom field definitions of any Trello Board")
-for df in cfd:
-    if df['type'] == 'text' :
-        collect[df['name']] = form1.text_input(df['name'])
-    elif df['type'] == 'checkbox' :
-        collect[df['name']] = form1.checkbox(df['name'], value=False)
-    elif df['type'] == 'date' :
-        date = form1.date_input("Enter date for {}".format(df['name']))
-        time = form1.time_input("Enter time for {}".format(df['name']))
-        collect[df['name']] = "{}T{}".format(date, time)
-    elif df['type'] == 'list' :
-        options = [choice['value']['text'] for choice in df['options']]
-        collect[df['name']] = form1.selectbox(df['name'], options=options)
-    elif df['type'] == 'number' :
-        collect[df['name']] = round(form1.number_input(df['name'],step=0.1), 2)
-    # Every form must have a submit button.
-
-ready = form1.form_submit_button("Submit")
-
-if ready:
-    st.json(collect)
-    res_update = requests.post('https://bpqc1s.deta.dev/update', json=collect)
-    if res_update.status_code == 200:
-        st.session_state['card_id'] = res_update.json()['card_id']
-        card_id = res_update.json()['card_id']
-        order.put({"line_items" : []},card_id)
-    else:
-        st.error(res_update.text)
-
-
-    st.header("You can now add order items to be stored in Deta Base")
-
-    items = []
-    last_order = 0
-    res = order.get(card_id)
-    st.write(res)
-
-    if res['line_items'] != None :
-        items = res['line_items']
-        last_order = len(items)
-
-        more = "Yes""""
 more = True
 while more:
     form2 = st.form("Order Details", clear_on_submit=True)
