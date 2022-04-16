5972b1cea040ab4a87d5b1b63c11f84b6bd77a12
Sunny Lim <limhss@gmail.com>
2022-04-16 21:35:06 +0800

    S

--- m_trello_deta_form.py
+++ m_trello_deta_form.py
@@ -48,7 +48,13 @@ if st.session_state['more'] == "Yes" :
             if last == "Yes" :
                 st.session_state['more'] = "No"
                 del st.session_state['items']
-st.header("Create a card")
-del st.session_state['more']
-st.text_input("Card Name")
-st.text_area("Card Description")
+                st.header("Create a card")
+                del st.session_state['more']
+                collect['board_id'] = board_id
+                collect['cardname'] = st.text_input('Card Name')
+                collect['carddescription'] = st.text_area('Card Description')
+                res_update = requests.post('https://bpqc1s.deta.dev/update', json=collect)
+                if res_update.status_code == 200:
+                    order.put({"line_item" : items}, res_update.json()['card_id'])
+                else:
+                    st.error(res_update.text)
