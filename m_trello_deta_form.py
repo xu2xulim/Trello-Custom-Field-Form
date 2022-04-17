7928ac751594bb42ca0219d37627050653827dcc
Sunny Lim <limhss@gmail.com>
2022-04-17 15:09:46 +0800

    S

--- m_trello_deta_form.py
+++ m_trello_deta_form.py
@@ -54,7 +54,7 @@ if st.session_state['focus'] == 1:
                     if last == "Yes" :
                         st.session_state['more'] = "No"
                         st.session_state['focus'] = 2
-
+                        st.write(st.session_state)
 
 
 
