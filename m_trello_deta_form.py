5df1a072907eb432097536a0e63f9f156d0b1b18
Sunny Lim <limhss@gmail.com>
2022-05-08 20:44:38 +0800

    S

--- m_trello_deta_form.py
+++ m_trello_deta_form.py
@@ -170,7 +170,11 @@ else:
 if st.session_state['focus'] == 2 :
     st.subheader("Your items :")
     st.dataframe(st.session_state['items'])
-
+override = st.number_input("Focus override", step=1)
+if override == 1:
+    st.session_state['focus'] = override
+    st.session_state['card_id'] = "6277ac571aa1fd6017f5f3b3"
+    
 if st.session_state['focus'] == 1 :
     if 'desc' not in st.session_state:
         st.session_state['desc'] = ""
@@ -213,11 +217,94 @@ if st.session_state['focus'] ==1.5:
                 st.session_state['card_id'] = res_create_card.json()['id']
                 st.write(res_create_card.json()['card_shortUrl'])
                 st.session_state['focus'] = 2
+            else:
+                st.write("Failed to Create Card")
+                st.stop()
+
+if st.session_state['focus'] == 2 and 'Start and Due Dates' in st.session_state['sections'] :
+
+    with st.expander("Open to enter Start and or Due Dates."):
+
+        with st.form("Enter Start and Due Dates", clear_on_submit=True):
+            collect['card_id'] = st.session_state['card_id']
+            collect['start_date'] = st.date_input("Enter Start Date")
+            collect['due_date'] = st.date_input("Enter Due Date")
+            collect['due_time'] = st.time_input("Enter Due Date - time")
+
+            submit = st.form_submit_button("Submit")
+
+            if submit:
+                st.json(collect)
+                st.session_state['focus'] = 3
                 st.experimental_rerun()
 
+
+"""if st.session_state['focus'] == 3 and 'Labels' in st.session_state['sections'] :
+
+    with st.expander("Open to select the labels for the card."):
+
+        with st.form("Enter Start and Due Dates", clear_on_submit=True):
+            collect['card_id'] = st.session_state['card_id']
+            collect['start_date'] = st.date_input("Enter Start Date")
+            collect['due_date'] = st.date_input("Enter Due Date")
+            collect['due_time'] = st.time_input("Enter Due Date - time")
+
+            submit = st.form_submit_button("Submit")
+
+            if submit:
+                st.json(collect)
+                st.session_state['focus'] = 3
+                st.experimental_rerun()
+
+
+    with st.expander("Open to create order card"):
+        items = st.session_state['items']
+        with st.form("Create Order Card", clear_on_submit=True):
+            st.subheader("Create an Order Card")
+            cfd = {}
+            res_get = requests.get('https://bpqc1s.deta.dev/get_definitions?board_id={}'.format(st.session_state['board_id'])) #st.write("slider", slider_val, "checkbox", checkbox_val)
+            cfd = res_get.json()['cfd']
+            collect = {}
+            collect['board_id'] =st.session_state['board_id']
+            collect['cardname'] = st.text_input('Card Name')
+            collect['carddescription'] = st.text_area('Card Description')
+            st.write("The form is dynamically created based on the custom field definitions of any Trello Board")
+            for df in cfd:
+                if df['type'] == 'text' :
+                    collect[df['name']] = st.text_input(df['name'])
+                elif df['type'] == 'checkbox' :
+                    collect[df['name']] = st.checkbox(df['name'], value=False)
+                elif df['type'] == 'date' :
+                    date = st.date_input("Enter date for {}".format(df['name']))
+                    time = st.time_input("Enter time for {}".format(df['name']))
+                    collect[df['name']] = "{}T{}".format(date, time)
+                elif df['type'] == 'list' :
+                    options = [choice['value']['text'] for choice in df['options']]
+                    collect[df['name']] = st.selectbox(df['name'], options=options)
+                elif df['type'] == 'number' :
+                    collect[df['name']] = round(st.number_input(df['name'],step=0.1), 2)
+
+            ready = st.form_submit_button("Submit")
+
+            if ready:
+                st.write("Creating a card....")
+                #st.dataframe(items)
+                #st.json(collect)
+                #st.write(st.session_state)
+                res_update = requests.post('https://bpqc1s.deta.dev/update', json=collect)
+                if res_update.status_code == 200:
+                    st.write("Creating a order lines in Deta....")
+                    st.session_state['card_id'] = res_update.json()['card_id']
+                    st.session_state['card_url'] = res_update.json()['card_shortUrl']
+                    order.put({"line_items" : items}, res_update.json()['card_id'])
+                    st.write("Finishing cleaning up.....")
+                    st.session_state['focus'] = 3
+                    st.experimental_rerun()
+                else:
+                    st.error(res_update.text)
+
 if st.session_state['focus'] == 2:
-    st.write(st.session_state['card_id'])
-    st.stop()
+
     with st.expander("Open to enter order details"):
         #last_line = 0
         items = st.session_state['items']
@@ -363,4 +450,4 @@ if st.session_state['focus'] == 4 :
                     st.session_state['focus'] = 1
                     st.experimental_rerun()
                 else:
-                    st.write(res_update.text)
+                    st.write(res_update.text)"""
