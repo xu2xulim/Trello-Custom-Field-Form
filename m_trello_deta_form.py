9e6417cdd14f51326dca2eef5ed5efe08486b725
Sunny Lim <limhss@gmail.com>
2022-05-08 20:47:22 +0800

    S

--- m_trello_deta_form.py
+++ m_trello_deta_form.py
@@ -173,7 +173,7 @@ if st.session_state['focus'] == 2 :
 """override = st.number_input("Focus override", step=1)
 if override == 1:
     st.session_state['focus'] = override
-    st.session_state['card_id'] = "6277ac571aa1fd6017f5f3b3""""
+    st.session_state['card_id'] = "6277ac571aa1fd6017f5f3b3"""
 
 if st.session_state['focus'] == 1 :
     if 'desc' not in st.session_state:
